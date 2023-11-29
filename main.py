5b5a992a02777d8a97532c930f458f2cc911d326
tsetzer <t.setzer@setzermaps.de>
2023-11-29 18:10:13 +0700

    first links

--- main.py
+++ main.py
@@ -5,40 +5,31 @@ import datetime
 
 # Dictionary to store YouTube links
 youtube_links = {
-    1: "https://youtu.be/uMMscUKV70M",          # Karin
-    2: "https://youtu.be/CgcBhleSUNk",          # Malaysia
-    3:
-    4: "https://youtube.com/shorts/0Y10H_hkoKI",
-    5: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    6:
-    7: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    8: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    9:
-    10: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    11: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    12: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    13: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    14:
-    15: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    16: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    17: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    18: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    19: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    20: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    21: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    22: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    23: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
-    24: "https://youtube.com/shorts/0Y10H_hkoKI?feature=share",
+    1: "https://youtu.be/uMMscUKV70M",           # Karin
+    2: "https://youtu.be/PP7fQf0NTfo",           # Kreta
+    3: "https://youtu.be/MTwjOV1JrLM",           # Sascha
+    4: "https://youtu.be/tyUe_7oQKDE",           # Segeln
+    5: "https://youtu.be/q9BMYjceeMQ",           # Setzers
+    6: "https://youtube.com/shorts/he1RYZT_m1E?feature=share", # Julia & Lukas
+    7: "https://youtu.be/dmrDpjsm67g",           # West Coast
+    8: "https://youtu.be/zL_CbIS-i1I",           # Boston
+    9: "https://youtube.com/shorts/DoICWB4ZgQU?feature=share", # Regina
+    10: "https://youtu.be/tR882g-lCTs",           # Gardasee
+    11: "https://youtube.com/shorts/pRSlqy5rqY4?feature=share", # Sven
+    12: "https://youtu.be/CgcBhleSUNk",          # Malaysia
+    13: "https://youtube.com/shorts/Wdx6iojoOZE?feature=share", # Sonia
+    14: "https://youtu.be/m7N-XmOwrmg",          # Java
+    15: "https://youtube.com/shorts/nbwpvfsMsAc?feature=share", # Nga & Marchy
+    16: "https://youtu.be/wE9YZ4HH_70",          # Lombok
+    17: "https://youtu.be/Fg2jtjZTj70",          # Singapur
+    18: "",
+    19: "",
+    20: "",
+    21: "",
+    22: "",
+    23: "",
+    24: "",
 }
-
-
-# "https://youtu.be/uMMscUKV70M",          # Karin
-#
-# "https://youtu.be/CgcBhleSUNk",          # Malaysia
-# "https://youtu.be/m7N-XmOwrmg",          # Java
-# "https://youtu.be/wE9YZ4HH_70",          # Lombok
-# "https://youtu.be/Fg2jtjZTj70",          # Singapur
-
 
 def main():
 
@@ -47,7 +38,7 @@ def main():
     st.write("\n --- \n")
 
     # today = datetime.date.today()
-    today = datetime.date(2023,12,15)
+    today = datetime.date(2023,12,17)
     # st.write(today)
 
     for i in range(1, 25):
@@ -58,7 +49,7 @@ def main():
 
         else:
             # st.link_button(f"Türchen {i}: Noch geheim... 🤐🤶", youtube_links[i], disabled=True)
-            st.link_button(f"Türchen {i}", youtube_links[i], disabled=True)
+            st.link_button(f" Geheimes Türchen {i}", youtube_links[i], disabled=True)
 
 
 if __name__ == "__main__":
