ff78da5c613368ced2b5fb3cf6ff81033f432bdc
tsetzer <t.setzer@setzermaps.de>
2023-12-07 14:32:58 +0700

    additional tuerchen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-# import datetime
+import datetime
 from datetime import datetime, timedelta
 
 
@@ -14,33 +14,34 @@ youtube_links = {
     7: "https://youtube.com/shorts/he1RYZT_m1E?feature=share",      # Julia & Lukas
     8: "https://youtu.be/DC1w7Rv_lIc",           # Oma Guenter
     9: "https://youtu.be/aBTnvXFkxRY",           # Oma Dell
-    10: "https://youtu.be/dmrDpjsm67g",          # West Coast
+    10: "https://youtu.be/tyUe_7oQKDE",          # Segeln
     11: "https://youtube.com/shorts/nbwpvfsMsAc?feature=share",     # Nga & Marchy
-    12: "https://youtube.com/shorts/DoICWB4ZgQU?feature=share",     # Regina
-    13: "https://youtu.be/zL_CbIS-i1I",          # Boston
-    14: "https://youtube.com/shorts/pRSlqy5rqY4?feature=share",     # Sven
+    12: "https://youtube.com/shorts/DoICWB4ZgQU?feature=share",     # Regina & Sven (s. unten)
+    13: "https://youtu.be/dmrDpjsm67g",          # West Coast
+    14: "https://youtube.com/shorts/zpJobnbUGqI?feature=share",     # Maxi & Marius
     15: "https://youtu.be/X8QK55z2S8Y",          # Max & Franzi
     16: "https://youtube.com/shorts/Wdx6iojoOZE?feature=share",     # Sonia
-    17: "",
-    18: "",
+    17: "https://youtu.be/zL_CbIS-i1I",          # Boston,
+    18: "https://youtu.be/GtKEV6CyPoo",          # Claudia
     19: "https://youtu.be/WlNeWszFnIE",          # Elena Alex
     20: "https://youtu.be/tR882g-lCTs",          # Gardasee
     21: "https://youtube.com/shorts/LE3YHn2Vt48?feature=share",     # Alex und Anna
-    22: "https://youtu.be/CgcBhleSUNk",          # Malaysia
+    22: "",
     23: "",
-    24: "",
+    24: "https://youtu.be/CgcBhleSUNk",          # Malaysia"
 }
 
-# "https://youtu.be/m7N-XmOwrmg",          # Java
-# "https://youtu.be/wE9YZ4HH_70",          # Lombok
-# "https://youtu.be/Fg2jtjZTj70",          # Singapur
-# "https://youtu.be/tyUe_7oQKDE",          # Segeln
 
 def main():
 
     st.markdown("<h1 style='text-align: center'>🎄️❤️ Annas ❤️🎄</h1>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center'>🌨️❤️ Adventskalender ❤️🌨</h1>", unsafe_allow_html=True)
     st.write("\n --- \n")
+
+    ### DEBUG manual server_time for testing
+    # server_time = datetime(2022, 12, 28, 23, 55, 59, 342380)
+    # st.write(server_time)
+    ### DEBUG END
 
     # Get current server time and shift by X hours
     server_time = datetime.now()
@@ -53,6 +54,16 @@ def main():
         if i == 6 and today >= datetime(today.year, 12, i).date():
             st.link_button(f"Türchen {i}a", youtube_links[i])
             st.link_button(f"Türchen {i}b", "https://maps.app.goo.gl/QNHRA1ENLAf2cjNn7")
+
+        elif i == 12 and today >= datetime(today.year, 12, i).date():
+            st.link_button(f"Türchen {i}a", youtube_links[i])
+            st.link_button(f"Türchen {i}b", "https://youtube.com/shorts/pRSlqy5rqY4?feature=share")      # Sven
+
+        elif i == 24 and today >= datetime(today.year, 12, i).date():
+            st.link_button(f"Türchen {i}a", youtube_links[i])
+            st.link_button(f"Türchen {i}b", "https://youtu.be/m7N-XmOwrmg")         # Java
+            st.link_button(f"Türchen {i}c", "https://youtu.be/wE9YZ4HH_70")         # Lombok
+            st.link_button(f"Türchen {i}d", "https://youtu.be/Fg2jtjZTj70")         # Singapur
 
         elif today >= datetime(today.year, 12, i).date():
             st.link_button(f"Türchen {i}", youtube_links[i])
